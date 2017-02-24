0f5f852c6eb2049e65649ce3c5d5374f2ce8137b
Judah Meek <judah.meek@gmail.com>
2017-02-24 01:30:45 -0600

    _.uuid > _.uniqueId

--- todo-app-production/client/app/bundles/todosIndex/actions/tempTodos/index.js
+++ todo-app-production/client/app/bundles/todosIndex/actions/tempTodos/index.js
@@ -1,9 +1,9 @@
 // @flow
-import _ from 'lodash-uuid';
+import _ from 'lodash';
 import { createAction } from 'redux-actions';
 import actionTypes from './actionTypes';
 
 export const addTodo = createAction(actionTypes.ADD_TODO);
 export const addTempTodo = createAction(actionTypes.ADD_TEMP_TODO,
-  (description: string, id: string = `TEMP_TODO_${_.uuid}`) => ({ description, id }));
+  (description: string, id: string = _.uniqueId('TEMP_TODO_')) => ({ description, id }));
 export const toggleTodo = createAction(actionTypes.TOGGLE_TODO);
