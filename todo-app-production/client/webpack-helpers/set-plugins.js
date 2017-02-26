1a0b326c4272dbce87a5df2bc2dc7f301c3d1e9f
Judah Meek <judah.meek@gmail.com>
2017-02-26 01:30:06 -0600

    add Bootstrap 4

--- todo-app-production/client/webpack-helpers/set-plugins.js
+++ todo-app-production/client/webpack-helpers/set-plugins.js
@@ -12,6 +12,24 @@ function setPlugins(builderConfig, webpackConfig) {
   const ifChunk = option => addOption(builderConfig.chunk, option);
 
   const plugins = removeEmpty([
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      Util: 'exports-loader?Util!bootstrap/js/dist/util',
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: getEnvVar('NODE_ENV'),
@@ -29,11 +47,9 @@ function setPlugins(builderConfig, webpackConfig) {
         // Workaround for `resolve-url-loader`:
         // https://github.com/bholloway/resolve-url-loader/issues/33#issuecomment-249830601
         output: {
-          path: (
-            builderConfig.hmr
+          path: builderConfig.hmr
             ? path.resolve(__dirname, '..', '..', 'public')
-            : path.resolve(__dirname, '..', '..', 'app', 'assets', 'webpack')
-          ),
+            : path.resolve(__dirname, '..', '..', 'app', 'assets', 'webpack'),
         },
 
         babel: {
@@ -59,23 +75,27 @@ function setPlugins(builderConfig, webpackConfig) {
       },
     }),
 
-    ifChunk(new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      chunks: [
-        'global-styles', // See ./set-entry.js
-        'listings-index',
-      ],
-      filename: 'vendor-client-bundle.js',
-      minChunks: Infinity,
-    })),
+    ifChunk(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        chunks: [
+          'global-styles', // See ./set-entry.js
+          'listings-index',
+        ],
+        filename: 'vendor-client-bundle.js',
+        minChunks: Infinity,
+      }),
+    ),
     ifHmr(new webpack.HotModuleReplacementPlugin()),
     ifHmr(new webpack.NoEmitOnErrorsPlugin()),
-    ifOptimize(new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        warnings: true,
-      },
-    })),
+    ifOptimize(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: true,
+        },
+      }),
+    ),
     ifExtractText(new ExtractTextPlugin({ filename: '[name]-client-bundle.css', allChunks: true })),
   ]);
 
