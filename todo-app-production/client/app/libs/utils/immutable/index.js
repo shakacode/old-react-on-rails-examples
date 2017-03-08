397033c0ee8976a917baa4ed6ff0d56b717ebba5
Judah Meek <judah.meek@gmail.com>
2017-03-07 22:59:06 -0600

    incorporate reselect

---
+++ todo-app-production/client/app/libs/utils/immutable/index.js
@@ -0,0 +1,52 @@
+import { Map as $$Map } from 'immutable';
+import _ from 'lodash/fp';
+
+import { toArray } from 'app/libs/utils';
+
+type Immutable = $$Map;
+type Path = Array<string | number>;
+
+// calls `toJS` if given object is immutable, returns the object unchanged otherwise
+export const toJS = (obj: {}) => (_.isFunction(_.get('toJS', obj)) ? obj.toJS() : obj);
+
+// adds an item to a nested immutable $$Set
+export const unionIn = _.curry(
+  (path: Path, item: any, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedSet => $$nestedSet.union(toArray(item))),
+);
+
+// removes an item from a nested immutable $$Set
+export const subtractIn = _.curry(
+  (path: Path, item: any, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedSet => $$nestedSet.subtract(toArray(item))),
+);
+
+// clears a nested immutable $$Set
+export const clearIn = _.curry(
+  (path: Path, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedSet => $$nestedSet.clear()),
+);
+
+// pushes an item to a nested $$List
+export const pushIn = _.curry(
+  (path: Path, item: any, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedList => $$nestedList.push(item)),
+);
+
+// deletes an item from a nested $$List
+export const deleteIn = _.curry(
+  (path: Path, targetIndex: number, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedList => $$nestedList.delete(targetIndex)),
+);
+
+// removes items in nested $$List for which predicate returns true
+export const rejectIn = _.curry(
+  (path: Path, predicate: Function, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedList => $$nestedList.filterNot(predicate)),
+);
+
+// removes items in nested $$List for which predicate returns false
+export const filterIn = _.curry(
+  (path: Path, predicate: Function, $$immutable: Immutable) =>
+    $$immutable.updateIn(path, $$nestedList => $$nestedList.filter(predicate)),
+);
