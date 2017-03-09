08125fd5f5e4b8935c30059aaf9107444cca14bd
Judah Meek <judah.meek@gmail.com>
2017-03-09 04:32:19 -0600

    add DevTools

--- todo-app-production/client/app/bundles/todosIndex/startup/App.jsx
+++ todo-app-production/client/app/bundles/todosIndex/startup/App.jsx
@@ -1,11 +1,10 @@
 // @flow
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReactOnRails from 'react-on-rails';
 import { Provider } from 'react-redux';
+
+import DevTools from 'app/libs/utils/DevTools';
 
 import store from '../store';
 import AddTodoFormContainer from '../containers/AddTodoFormContainer';
@@ -14,18 +13,20 @@ import TodoListContainer from '../containers/TodoListContainer';
 
 const App = (railsProps: {}, _railsContext) => (
   <Provider store={store(railsProps)}>
-    <Router basename="/todos">
-      <Route path="/:filter">
-        {
-          (_matchProps) =>
+    <div>
+      <Router basename="/todos">
+        <Route path="/:filter">
+          {_matchProps => (
             <div>
               <AddTodoFormContainer />
               <NavLinks />
               <TodoListContainer />
             </div>
-        }
-      </Route>
-    </Router>
+          )}
+        </Route>
+      </Router>
+      <DevTools />
+    </div>
   </Provider>
 );
 
--- todo-app-production/client/app/bundles/todosIndex/store/index.js
+++ todo-app-production/client/app/bundles/todosIndex/store/index.js
@@ -2,6 +2,8 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
+import DevTools from 'app/libs/utils/DevTools';
+
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 import composeInitialState from './composeInitialState';
@@ -12,7 +14,7 @@ const initializeStore = (railsProps: {}) => {
   const store = createStore(
     rootReducer,
     composeInitialState(railsProps),
-    compose(applyMiddleware(sagaMiddleware)),
+    compose(applyMiddleware(sagaMiddleware), DevTools.instrument()),
   );
 
   sagaMiddleware.run(rootSaga);
