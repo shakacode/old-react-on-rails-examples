70f1ffc3b12fde9f79ba33457eb872254b0b8d8e
Igor Krtolica <igor@blinkinc.com>
2017-03-10 12:59:02 -0800

    Cleanup from merge

--- todo_app_react_native/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import todos from './todos';
-import visbilityFilter from './visbilityFilter';
-
-export default combineReducers( {
-  todos,
-  visbilityFilter,
- } );
