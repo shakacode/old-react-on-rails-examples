05c848611feda897a83e698506dedc352cc79355
Judah Meek <judah.meek@gmail.com>
2017-03-15 01:39:25 -0500

    remove unused dependencies

--- todo-app-production/client/app/libs/utils/DevTools.jsx
+++ todo-app-production/client/app/libs/utils/DevTools.jsx
@@ -1,29 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
-import Dispatcher from 'redux-devtools-dispatch';
-import MultipleMonitors from 'redux-devtools-multiple-monitors';
-import SliderMonitor from 'redux-slider-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
 import DiffMonitor from 'redux-devtools-diff-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
-import ChartMonitor from 'redux-devtools-chart-monitor';
-
-import * as todoActionCreators from 'todosIndex/actions/todos';
-import * as formActionCreators from 'todosIndex/actions/forms';
+import LogMonitor from 'redux-devtools-log-monitor';
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q" changeMonitorKey="ctrl-m">
-    <ChartMonitor />
     <LogMonitor />
+    <ChartMonitor />
     <DiffMonitor />
   </DockMonitor>,
 );
-
-/*
-    // ref errors
-    <Dispatcher actionCreators={{ todos: todoActionCreators, forms: formActionCreators }} initEmpty />
-    <SliderMonitor keyboardEnabled />
-*/
-
-// TODO: add inspector and upgrade log-monitor to filterable-log-monitor
