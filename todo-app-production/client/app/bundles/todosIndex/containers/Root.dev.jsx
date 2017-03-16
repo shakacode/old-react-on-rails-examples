f0e517f6cbc365dcb58e8a99fc660512aa3367a7
Judah Meek <judah.meek@gmail.com>
2017-03-16 00:38:22 -0500

    split Provider as well

--- todo-app-production/client/app/bundles/todosIndex/containers/Root.dev.jsx
+++ todo-app-production/client/app/bundles/todosIndex/containers/Root.dev.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import DevTools from 'app/libs/utils/DevTools';
 
@@ -8,21 +9,23 @@ import AddTodoFormContainer from '../containers/AddTodoFormContainer';
 import NavLinks from '../components/NavLinks';
 import TodoListContainer from '../containers/TodoListContainer';
 
-const Root = (wat: number) => (
-  <div>
-    <Router basename="/todos">
-      <Route path="/:filter">
-        {_matchProps => (
-          <div>
-            <AddTodoFormContainer />
-            <NavLinks />
-            <TodoListContainer />
-          </div>
-        )}
-      </Route>
-    </Router>
-    <DevTools />
-  </div>
+const Root = ({ store }: { store: {} }) => (
+  <Provider store={store}>
+    <div>
+      <Router basename="/todos">
+        <Route path="/:filter">
+          {_matchProps => (
+            <div>
+              <AddTodoFormContainer />
+              <NavLinks />
+              <TodoListContainer />
+            </div>
+          )}
+        </Route>
+      </Router>
+      <DevTools />
+    </div>
+  </Provider>
 );
 
 export default Root;
--- todo-app-production/client/app/bundles/todosIndex/startup/App.jsx
+++ todo-app-production/client/app/bundles/todosIndex/startup/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactOnRails from 'react-on-rails';
-import { Provider } from 'react-redux';
 
 import Root from '../containers/Root';
 import store from '../store';
 
-const App = (railsProps: {}, _railsContext) => (
-  <Provider store={store(railsProps)}>
-    <Root />
-  </Provider>
-);
+const App = (railsProps: {}, _railsContext) => <Root store={store(railsProps)} />;
 
 ReactOnRails.register({ App });
