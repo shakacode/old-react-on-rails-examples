5656d1e64757a4f989057ae6839c9fb299df9acc
Judah Meek <judah.meek@gmail.com>
2017-03-28 02:22:16 -0500

    add flow annotations

--- todo-app-production/client/app/bundles/todosIndex/store/composeInitialState.js
+++ todo-app-production/client/app/bundles/todosIndex/store/composeInitialState.js
@@ -2,9 +2,10 @@
 import { normalizeArrayToMap } from 'app/libs/utils/normalizr';
 
 import { rootReducerInitialState } from '../reducers';
+import type { Todo } from '../types';
 
 // TODO: hydrate initial state with rails props
-const composeInitialState = railsProps => {
+const composeInitialState = (railsProps: Array<Todo>) => {
   Object.assign(rootReducerInitialState.todos, normalizeArrayToMap(railsProps));
   return rootReducerInitialState;
 };
--- todo-app-production/client/app/bundles/todosIndex/store/store.dev.js
+++ todo-app-production/client/app/bundles/todosIndex/store/store.dev.js
@@ -7,10 +7,11 @@ import DevTools from 'app/libs/utils/DevTools';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 import composeInitialState from './composeInitialState';
+import type { Todo } from '../types';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const initializeStore = (railsProps: {}) => {
+const initializeStore = (railsProps: Array<Todo>) => {
   const store = createStore(
     rootReducer,
     composeInitialState(railsProps),
