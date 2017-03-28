37522c10182107628787591345238a341bcb9267
Judah Meek <judah.meek@gmail.com>
2017-03-28 01:26:39 -0500

    add props to store

--- todo-app-production/client/app/bundles/todosIndex/store/composeInitialState.js
+++ todo-app-production/client/app/bundles/todosIndex/store/composeInitialState.js
@@ -4,6 +4,9 @@ import { normalizeArrayToMap } from 'app/libs/utils/normalizr';
 import { rootReducerInitialState } from '../reducers';
 
 // TODO: hydrate initial state with rails props
-const composeInitialState = railsProps => Object.assign(rootReducerInitialState.todos, normalizeArrayToMap(railsProps));
+const composeInitialState = railsProps => {
+  Object.assign(rootReducerInitialState.todos, normalizeArrayToMap(railsProps));
+  return rootReducerInitialState;
+};
 
 export default composeInitialState;
